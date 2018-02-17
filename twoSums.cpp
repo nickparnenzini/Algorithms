119fbd3e059371d37f26446f47934250452e5ba9
vparnenzini <valeria-parnenzini@users.noreply.github.com>
2018-02-17 22:32:28 +0000

    Create twoSums.cpp

---
+++ twoSums.cpp
@@ -0,0 +1,20 @@
+/* O(n) solution by using two indices */
+vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> result;
+    int left = 0;
+    int right = nums.size() - 1;
+    while (left < right) {
+        if (nums[left] + nums[right] == target) {
+            result.push_back(left+1);
+            result.push_back(right+1);
+            left++; right--;
+        }
+        if (nums[left] + nums[right] > target) {
+            right--;
+        }
+        if (nums[left] + nums[right] < target) {
+            left++;
+        }
+    }
+    return result;    
+}
