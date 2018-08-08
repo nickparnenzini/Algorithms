6071012cca384eae2d4a9f59d9dd5fa2e70bcc3f
vparnenzini <valeria-parnenzini@users.noreply.github.com>
2018-08-08 18:35:31 +0200

    Create dutchFlagProblem.cpp

---
+++ dutchFlagProblem.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+void dutchFlagProblem(vector<int>& A, int index) {
+    int pivot = A[A.size()/2];
+    int low = 0;
+    int high = A.size() - 1;
+    while (low < high) {
+      while(A[low] < pivot) {low++;}
+      while (A[high] > pivot) {high--;}
+      if ( low > high) {
+        break;
+      }
+      std::swap(A[low], A[high]);
+      low++;
+      high--;
+    } 
+}
+
+int main()
+{
+    std::vector<int> A = {3,1,2,0,2,1,1};
+    dutchFlagProblem(A, 3);
+    for (auto i : A) {
+      std::cout << i << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
